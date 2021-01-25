c176eda67cdc538368510afa9f8bd54ec85ba443
Yuliana Alvarado <yalvarado924@gmail.com>
2021-01-25 15:24:37 -0600

    commit tree f16a43b05b55579e4cdc930768d6c557b635b0d4

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        actors.map do |a|
+            a.full_name
+        end
+    end
 end
